ece3793be1004f12e0306845a348abe7a18f0a1b
craig91 <craigdejean1@gmail.com>
2017-05-04 19:06:01 -0400

    new sequelize environment variable

--- models/index.js
+++ models/index.js
@@ -11,8 +11,14 @@ var db        = {};
 
 console.log(env)
 console.log(config)
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (process.env.DATABASE_URL) {
+  var sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+  })
 } else {
   var sequelize = new Sequelize('postgres://fxyjrfsfbacgmq:e94d5fb003070e5d576a468569cedb4c0a0b5eba7b10d57d5393eab7c643d032@ec2-23-23-220-163.compute-1.amazonaws.com:5432/dbpjfpp41fret0');
 }
